6abd40aa8c157f3860e0daec326ef5fccb44f3a8
rosshaig <rosshaig@Rosss-MBP.lan>
2015-11-08 16:12:32 +0000

    Getting there

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 jQuery(document).ready(function($){
+
+
+	$(window).scroll(function() {
+	if ($(this).scrollTop() > 500){
+	    $('header').addClass("sticky");
+	  }
+	  else{
+	    $('header').removeClass("sticky");
+	  }
+	});
+
+
+
+
 	var timelineBlocks = $('.cd-timeline-block'),
 		offset = 0.8;
 
@@ -28,8 +42,88 @@ jQuery(document).ready(function($){
 	var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
 
 	// build scenes
-	new ScrollMagic.Scene({triggerElement: ".parallax-parent"})
-		.setTween(".parallax-image", {y: "80%", ease: Linear.easeNone})
+	new ScrollMagic.Scene({triggerElement: "#parallax-intro"})
+		.setTween("#parallax-intro > div", {y: "80%", ease: Linear.easeNone})
 		.addTo(controller);
+
+	new ScrollMagic.Scene({triggerElement: "#parallax-podcast"})
+		.setTween("#parallax-podcast > div", {y: "80%", ease: Linear.easeNone})
+		.addTo(controller);
+
+		new ScrollMagic.Scene({triggerElement: "#parallax-food"})
+			.setTween("#parallax-food > div", {y: "80%", ease: Linear.easeNone})
+			.addTo(controller);
+
+		var controller = new ScrollMagic.Controller();
+
+		// // define movement of panels
+		// var wipeAnimation = new TimelineMax()
+		// 	// .fromTo("section.panel.turqoise", 1, {x: "-100%"}, {x: "0%", ease: Linear.easeNone})  // in from left
+		// 	// .fromTo("section.panel.green",    1, {x:  "100%"}, {x: "0%", ease: Linear.easeNone})  // in from right
+		// 	// .fromTo("section.inequality", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone}) // in from top
+		// 	.fromTo("section.issue.podcast", 1, {y: "-100%"}, {y: "0%", ease: Linear.easeNone}) // in from top
+		// 	.fromTo("section.issue.wellbeing", 1, {y: "-100%"}, {y: "0%", ease: Linear.easeNone}); // in from top
+		//
+		// // create scene to pin and link animation
+		// new ScrollMagic.Scene({
+		// 		triggerElement: "#pinContainer",
+		// 		triggerHook: "onLeave",
+		// 		duration: "300%"
+		// 	})
+		// 	.setPin("#pinContainer")
+		// 	.setTween(wipeAnimation)
+		// 	.addTo(controller);
+
+
+
+
+//LETTER
+
+var flightpath = {
+			entry : {
+				curviness: 1.25,
+				autoRotate: true,
+				values: [
+						{x: 100,	y: -20},
+						{x: 300,	y: 10}
+					]
+			},
+			looping : {
+				curviness: 1.25,
+				autoRotate: true,
+				values: [
+						{x: 510,	y: 60},
+						{x: 620,	y: -60},
+						{x: 500,	y: -100},
+						{x: 380,	y: 20},
+						{x: 500,	y: 60},
+						{x: 580,	y: 20},
+						{x: 620,	y: 15}
+					]
+			},
+			leave : {
+				curviness: 1.25,
+				autoRotate: true,
+				values: [
+						{x: 660,	y: 20},
+						{x: 800,	y: 130},
+						{x: $(window).width() + 300,	y: -100},
+					]
+			}
+		};
+		// init controller
+		var controller = new ScrollMagic.Controller();
+
+		// create tween
+		var tween = new TimelineMax()
+			.add(TweenMax.to($("#plane"), 1.2, {css:{bezier:flightpath.entry}, ease:Power1.easeInOut}))
+			.add(TweenMax.to($("#plane"), 2, {css:{bezier:flightpath.looping}, ease:Power1.easeInOut}))
+			.add(TweenMax.to($("#plane"), 1, {css:{bezier:flightpath.leave}, ease:Power1.easeInOut}));
+
+		// build scene
+		var scene = new ScrollMagic.Scene({triggerElement: "#trigger", duration: 500, offset: 100})
+						.setPin("#target")
+						.setTween(tween)
+						.addTo(controller);
 
 });
